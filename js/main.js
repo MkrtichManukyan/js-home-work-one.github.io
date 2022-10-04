03a5c752f831004b871bac625b02513b6a66f7cb
Mkrtich <100070929+mkrtic@users.noreply.github.com>
2022-10-04 11:28:43 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let username = prompt("Ինչ է քո անունը ??");
+let userlastname = prompt("Ինչ է քո ազգանունը ??");
+let userage = prompt("Քանի տարեկան եք ??");
+
+
+alert(username + " " + userlastname  + " " + userage);
+console.log(username + " " + userlastname  + " " + userage);
+document.write(username + " " + userlastname  + " " + userage);
